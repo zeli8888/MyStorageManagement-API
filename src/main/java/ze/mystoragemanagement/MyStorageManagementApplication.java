8353c4d16971fe6886d3438fe5597c50993149ba
Edwin <lize201@mails.ucas.ac.cn>
2025-02-10 22:05:04 +0000

    init

---
+++ src/main/java/ze/mystoragemanagement/MyStorageManagementApplication.java
@@ -0,0 +1,13 @@
+package ze.mystoragemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyStorageManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyStorageManagementApplication.class, args);
+    }
+
+}
